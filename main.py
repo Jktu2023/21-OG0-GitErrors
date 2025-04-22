4dfed2851c4d10b85c12026d17ec91972e939b28
Jktu2023 <161673133+Jktu2023@users.noreply.github.com>
2025-04-22 14:25:46 +0700

    cat name

---
+++ main.py
@@ -0,0 +1,12 @@
+name = 'TryZorro'
+
+def prn_dog_name(name):
+    print(f'The dog name is {name}')
+
+prn_dog_name(name)
+
+
+def prn_cat_name(name):
+    print(f'The big cat name is {name}')
+
+prn_cat_name(name)
